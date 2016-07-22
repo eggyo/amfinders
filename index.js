8ddecaf50d95c4432f2b2be7c1f9527cbc4e41d0
eggyo <eggyoyo4859@gmail.com>
2016-07-22 13:39:49 +0700

    add fb app id

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  restAPIKey: process.env.REST_KEY,
+  facebookAppIds: process.env.FACEBOOK_APP_ID,
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
