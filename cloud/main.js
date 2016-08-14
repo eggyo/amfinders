e869f87eea4d967ae1723345e42d106057b38df7
eggyo <eggyoyo4859@gmail.com>
2016-08-14 21:39:25 +0700

    add push cloudcode

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,27 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+// iOS push testing
+Parse.Cloud.define("sendPush", function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user
+  var params = request.params;
+  var chanals = request.chanals;
+  // Our "Message" class has a "text" key with the body of the message itself
+  var message = params.message;
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.containedIn('chanals', [chanals]); // targeting iOS devices only
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query
+    data: message
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
