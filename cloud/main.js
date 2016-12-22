2e197cdbc8bae984ed8a2795ac4ad85ea0aef16e
eggyo <eggyoyo4859@gmail.com>
2016-12-22 13:06:34 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -73,6 +73,50 @@ Parse.Cloud.define("sendMessage", function(request, response) {
       response.error(error.message);
     }
   });
+});
 
+Parse.Cloud.define("retrieveAllObjects", function(request, status) {
+                       var result     = [];
+                       var chunk_size = 1000;
+                       var processCallback = function(res) {
+                           result = result.concat(res);
+                           if (res.length === chunk_size) {
+                               process(res[res.length-1].id);
+                           } else {
+                               status.success(result);
+                           }
+                       };
+                       var process = function(skip) {
+                           var query = new Parse.Query(request.params.className);
+                           if (skip) {
+                               query.greaterThan("objectId", skip);
+                           }
+                           if (request.params.update_at) {
+                               query.greaterThan("updatedAt", request.params.updateAt);
+                           }
+                           if (request.params.onlyObjectId) {
+                               query.select("objectId");
+                           }
+
+                           query.limit(chunk_size);
+                           query.ascending("objectId");
+                           query.find().then(function (res) {
+                               processCallback(res);
+                           }, function (error) {
+                               status.error("query unsuccessful, length of result " + result.length + ", error:" + error.code + " " + error.message);
+                           });
+                       };
+                       process(false);
+});
+
+Parse.Cloud.define("getChannelsFeed", function(request, response) {
+  var query = new Parse.Query("Channels");
+  query.ascending("updatedAt");
+  query.ascending("member");
+  query.find().then(function (res) {
+      response.success(res);
+  }, function (error) {
+      response.error("getChannelsFeed unsuccessful, error:" + error.code + " " + error.message);
+  });
 
 });
